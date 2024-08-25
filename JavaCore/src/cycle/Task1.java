e6e9b62459077b4f331c19c676eb760f1f16c707
Milan Nasrytinov <n.milan1989@gmail.com>
2024-08-25 23:08:00 +0600

    cycle task1,2

---
+++ JavaCore/src/cycle/Task1.java
@@ -0,0 +1,16 @@
+package cycle;
+
+import java.util.Scanner;
+
+public class Task1 {
+    public static void main(String[] args) {
+        Scanner in = new Scanner("6 7 3 2 9 5 4 1 7 8");
+        int count = 0;
+        while (in.hasNextInt()) {
+            if (in.nextInt() < 4) {
+                count++;
+            }
+        }
+        System.out.println(count);
+    }
+}
---
+++ JavaCore/src/cycle/Task2.java
@@ -0,0 +1,24 @@
+package cycle;
+
+import java.util.Scanner;
+
+public class Task2 {
+    public static void main(String[] args) {
+        Scanner in = new Scanner("7 3");
+        int a = in.nextInt();
+        int b = in.nextInt();
+        int c;
+        if (a > b) {
+            c = a;
+            a = b;
+            b = c;
+        }
+        double num = a;
+
+        while (a < b) {
+            num *= a + 1;
+            ++a;
+        }
+        System.out.println(num);
+    }
+}
