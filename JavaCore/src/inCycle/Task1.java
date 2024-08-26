ac378297c0142e2debb72a7fbd03bb191307a356
Milan Nasrytinov <n.milan1989@gmail.com>
2024-08-26 22:22:53 +0600

    debug, inCycle

---
+++ JavaCore/src/inCycle/Task1.java
@@ -0,0 +1,16 @@
+package inCycle;
+
+import java.util.Scanner;
+
+public class Task1 {
+    public static void main(String[] args) {
+        int num = new Scanner(System.in).nextInt();
+
+        for (int i = num; i >= 1; i--) {
+            for (int j = num; j >= num; j--) {
+
+            }
+            System.out.print(s);
+        }
+    }
+}
