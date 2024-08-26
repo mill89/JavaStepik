c42e58128e2d029fb79eadf907f07d6ac2217cca
Milan Nasrytinov <n.milan1989@gmail.com>
2024-08-26 15:17:01 +0600

    cycle task5,6

---
+++ JavaCore/src/cycle/Task5.java
@@ -0,0 +1,17 @@
+package cycle;
+
+import java.util.Scanner;
+
+public class Task5 {
+    public static void main(String[] args) {
+        Scanner in = new Scanner("17 2 3 4 9 -1");
+        int control_num = in.nextInt();
+        int div = 0;
+        int num = in.nextInt();
+        do {
+            if (control_num % num == 0) div++;
+            num = in.nextInt();
+        } while (num > 0);
+        System.out.println(div);
+    }
+}
