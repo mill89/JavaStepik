19de0ea0452d6a90765e8b253fd17c8ff636b810
Milan Nasrytdinov <milan.nasrytdinov@gmail.com>
2024-08-28 22:11:20 +0600

    inCycle task5,6

--- JavaCore/src/inCycle/Task4.java
+++ JavaCore/src/inCycle/Task4.java
@@ -2,16 +2,15 @@ package inCycle;
 
 import java.util.Scanner;
 
-public class Task3 {
+public class Task4 {
     public static void main(String[] args) {
-        int height = new Scanner(System.in).nextInt();
+        int height = new Scanner("7").nextInt();
         if (height <= 0) {
             System.out.println("ERROR");
             return;
         }
 
-
-        for (int i = 1; i <= height; i++) {
+        for (int i = 1; i <= height; i++, height--) {
             for (int j = 1; j <= height; j++) {
                 if (j < i) {
                     System.out.print(" ");
