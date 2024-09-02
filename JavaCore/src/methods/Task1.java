19506c8a05cb23c8bc90636110e72964a50c05d3
Milan Nasrytinov <n.milan1989@gmail.com>
2024-09-02 19:26:11 +0600

    methods > task1

---
+++ JavaCore/src/methods/Task1.java
@@ -0,0 +1,28 @@
+package methods;
+
+import java.util.Scanner;
+
+public class Task1 {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner("127 69 -3 0 7"); // true false false false true
+        for (int i = 0; i < 5; i++) {
+            int number = scan.nextInt();
+            System.out.print(isPrime(number) + " ");
+
+        }
+    }
+
+    // проверка на простое число
+    static boolean isPrime(int num) {
+        // 1 и 0 не простые числа
+        if (num == 1 || num <= 0) return false;
+        // проверка чисел, кроме 1 и последнего индекса
+        for (int i = 2; i < num; i++) {
+            if (num % i == 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
+
