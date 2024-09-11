eac556be827bc1b122dc268efffdecb441d13109
Milan Nasrytinov <n.milan1989@gmail.com>
2024-09-11 22:17:04 +0600

    ArraysAndMethods > task2,3

--- JavaCore/src/javabase/eskova/string/Task2ReplaceChars.java
+++ JavaCore/src/javabase/eskova/string/Task2ReplaceChars.java
@@ -1,20 +1,11 @@
 package javabase.eskova.string;
 
-import java.util.Arrays;
 import java.util.Scanner;
 
 public class Task2ReplaceChars {
     public static void main(String[] args) {
         Scanner in = new Scanner("hello;; by!; ;;");
         String str = in.nextLine();
-        String nStr;
-        nStr = str.replace(';', '.');
-        while (str.indexOf(';') > 0){
-            int i = str.indexOf(';');
-            str.charAt(i)=".,";
-        }
-
-
-        System.out.println(nStr);
+        System.out.println(str.replace(";", ",."));
     }
 }
---
+++ JavaCore/src/javabase/eskova/string/Task3CodewordJoin.java
@@ -0,0 +1,22 @@
+package javabase.eskova.string;
+
+
+import java.util.Scanner;
+
+public class Task3CodewordJoin {
+    public static void main(String[] args) {
+        Scanner in = new Scanner("""
+                To be or ;not; to be?
+                tes;ting; is good
+                I like ;ham; more then eggs""");
+
+        String psw = "";
+        while (in.hasNextLine()) {
+            String str = in.nextLine();
+            int begin = str.indexOf(';');
+            int end = str.indexOf(';');
+            psw += str.substring(begin + 1, end);
+        }
+        System.out.println(psw);
+    }
+}
